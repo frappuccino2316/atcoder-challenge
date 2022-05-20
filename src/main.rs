b235ec743cb37eba2b02dc086f52d1332a8e45e3
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-20 14:21:47 +0900

    ２で割れる回数

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: i64,
-        b: i64,
+        n: usize,
+        a: [i64; n],
     }
-    let r = if (a * b) % 2 == 0 {
-        String::from("Even")
-    } else {
-        String::from("Odd")
-    };
-    println!("{}", r);
+
+    let mut c = vec![];
+    for mut i in a {
+        let mut cnt = 0;
+        while i % 2 == 0 {
+            cnt += 1;
+            i /= 2;
+        }
+        c.push(cnt);
+    }
+
+    println!("{}", c.iter().min().unwrap());
 }
