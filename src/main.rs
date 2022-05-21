08a9f2eaf0bade0fe79732c8d865da7223e5a70e
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-21 22:10:45 +0900

    スロット

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,41 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
-        a: i64,
-        b: i64,
+        n: usize,
     }
 
-    let mut sum = 0;
-    for i in 1..=n {
-        let mut s = 0;
-        for j in i.to_string().chars() {
-            s += (j as i64) - 48;
+    let mut s = Vec::new();
+    for _ in 0..n {
+        input! {
+            si: String,
         }
-        if a <= s && s <= b {
-            sum += i;
+        let b: Vec<char> = si.chars().collect();
+        s.push(b);
+    }
+
+    let mut d = Vec::new();
+    for i in 0..=9 {
+        let mut second = 0;
+        let mut max_index = 0;
+
+        for (j, v) in s.iter().enumerate() {
+            for (k, e) in v.iter().enumerate() {
+                if i == (*e as i32) - 48 && max_index < k {
+                    max_index = k;
+                } else if i == (*e as i32) - 48 && max_index == k && j != 0 {
+                    second += 10;
+                }
+            }
+        }
+        d.push(second + max_index);
+    }
+
+    let mut m = 100;
+    for i in d {
+        if i < m {
+            m = i;
         }
     }
-    println!("{}", sum);
+
+    println!("{}", m);
 }
