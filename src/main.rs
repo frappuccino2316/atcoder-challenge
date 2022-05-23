24dfb09e54fc1e331477ce41b2c9375c477884ea
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-23 16:09:53 +0900

    252 D - Distinct Trio

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 use proconio::input;
-use proconio::marker::Chars;
-use std::cmp::max;
 
 fn main() {
     input! {
         n: usize,
-        s: [Chars; n],
+        a: [usize; n],
     }
 
-    let mut c = vec![vec![0_i32; 10]; 10];
+    let max = a.iter().max().unwrap();
+    let mut l = vec![0; max + 1];
 
-    for i in 0..n {
-        for j in 0..=9 {
-            c[(s[i][j] as usize) - 48][j] += 1;
+    for i in a {
+        l[i] += 1;
+    }
+
+    println!("{:?}", l);
+    let mut ans = (n * (n - 1) * (n - 2)) / 6;
+
+    for j in l {
+        if j > 2 {
+            ans -= (j * (j - 1) * (j - 2)) / 6;
+        }
+        if j > 1 {
+            ans -= ((j * (j - 1)) / 2) * (n - j);
         }
     }
 
-    let mut m = vec![0; 10];
-    for i in 0..=9 {
-        for j in 0..=9 {
-            m[i] = max(m[i], 10 * ((c[i][j] as i32) - 1) + j as i32);
-        }
-    }
-
-    println!("{}", m.iter().min().unwrap());
+    println!("{}", ans);
 }
