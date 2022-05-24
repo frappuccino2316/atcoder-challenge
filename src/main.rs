c6dea250c932ae3800916c72636d917092d74fb2
frappuccino2316 <frappuccino2316@gmail.com>
2022-05-24 15:17:58 +0900

    typical90 027 - Sign Up Requests

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,15 @@ use proconio::input;
 fn main() {
     input! {
         n: i64,
-        k: i64,
-        a: [i64; n],
-        b: [i64; n],
+        s: [String; n],
     }
 
-    let sa: i64 = a.iter().sum();
-    let sb: i64 = b.iter().sum();
+    let mut answer = Vec::new();
 
-    if (sa + k) % 2 == sb % 2 && (sb - sa).abs() <= k {
-        println!("Yes");
-    } else {
-        println!("No");
+    for (i, e) in s.iter().enumerate() {
+        if answer.is_empty() || answer.iter().all(|x| *x != e) {
+            println!("{}", i + 1);
+            answer.push(e);
+        }
     }
 }
