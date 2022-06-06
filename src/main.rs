d181f40487d45c8728d96ad34260b2f6aff8ced9
frappuccino2316 <frappuccino2316@gmail.com>
2022-06-07 00:44:52 +0900

    https://atcoder.jp/contests/abc253/tasks/abc253_b

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
 use proconio::input;
-use std::cmp::min;
-
-// 016 - Minimum Coins
-// https://atcoder.jp/contests/typical90/tasks/typical90_p
 
 fn main() {
     input! {
-        n: i64,
-        a: i64,
-        b: i64,
-        c: i64,
+        h: usize,
+        w: usize,
+        s: [String; h],
     }
 
-    let mut minimum = std::i64::MAX;
-    let limit = 9999;
+    let mut first = true;
+    let mut r = [0, 0];
 
-    let mut x = 0;
-    while x <= limit {
-        let mut y = 0;
-        while x + y <= limit {
-            let remaining = n - (a * x) - (b * y);
-            if remaining % c != 0 || remaining < 0 {
-                y += 1;
-                continue;
+    for (i, row) in s.iter().enumerate() {
+        for (j, c) in row.chars().enumerate() {
+            if c == 'o' {
+                if first {
+                    r[0] += i as i32;
+                    r[1] += j as i32;
+                    first = false;
+                } else {
+                    r[0] -= i as i32;
+                    r[1] -= j as i32;
+                }
             }
-            let z = remaining / c;
-            minimum = min(minimum, x + y + z);
-
-            y += 1;
         }
-        x += 1;
     }
 
-    println!("{}", minimum);
+    println!("{}", r[0].abs() + r[1].abs());
 }
